9061c6b3cd66c830a007a31ecfd121fbd155d92c
root <root@ip-172-31-38-138.ap-south-1.compute.internal>
2021-03-16 17:57:32 +0000

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+	    $table->string('mobile', 13)->unique();
+	    $table->enum('type', array('buyer','seller'))->default('buyer');
+            $table->timestamp('mobile_verified_at')->nullable();
+            $table->integer('otp');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
