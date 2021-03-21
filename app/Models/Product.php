862a581cb14595ed04ba17bbd9d9d21b522cec95
jasbirsmindfiresolutions <jasbirs@mindfiresolutions.com>
2021-03-21 14:16:38 +0530

    add product images model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $guarded = [];
+    protected $fillable = [
+        'category',
+        'user_id',
+        'name',
+        'description',
+        'price_per_kg',
+        'is_dynamic_price_enabled',
+        'is_out_of_stock',
+        'total_stock',
+    ];
 
 }
